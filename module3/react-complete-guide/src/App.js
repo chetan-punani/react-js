afc64b73f00fc4ef8a5c88e40517fda52f882c4f
chetan <chetan@gmail.com>
2022-07-22 10:17:40 +0530

    module 5 done.

--- module3/react-complete-guide/src/App.js
+++ module3/react-complete-guide/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
 import Person from './Person/Person.js';
+
+
+const StyleButton  = styled.button`
+background-color: ${props => props.alt ? 'red' : 'green'};
+color: white;
+font: inhirit;
+border: 3px solid blue;
+padding: 8px;
+cursor: pointer;
+
+&:hover {
+  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};;
+  color: black;
+}`;
 
 class App extends Component {
   state = {
@@ -24,14 +40,14 @@ class App extends Component {
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   }
 
   deletePersonHandler = (personIndex) => {
     // const person = this.state.persons.slice();
     const person = [...this.state.persons];
     person.splice(personIndex, 1);
-    this.setState({persons: person})
+    this.setState({ persons: person })
   }
 
   nameChangedHandler = (event, id) => {
@@ -48,33 +64,38 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons:persons});
+    this.setState({ persons: persons });
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inhirit',
-      border: '3px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inhirit',
+    //   border: '3px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let person = null;
 
-    if(this.state.showPersons) {
-        person = (
-          <div>
-            {
-              this.state.persons.map((person, index) => {
-                return <Person 
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  key={person.id}
-                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
-              })
-            }
+    if (this.state.showPersons) {
+      person = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
+            })
+          }
           {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
           <Person
             name={this.state.persons[1].name}
@@ -83,16 +104,33 @@ class App extends Component {
             changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
         </div>
-        )
+      )
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
     }
 
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+
     return (
-      <div className="App">
-        <h1>hi</h1>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>Tpggle Person</button>
+      <div>
+        <div className="App">
+          <h1>React APP</h1>
+          <p className={classes.join(' ')}>Working...</p>
+          <StyleButton 
+            alt={this.state.showPersons}
+            onClick={this.togglePersonHandler}>Tpggle Person</StyleButton>
           {person}
+        </div>
       </div>
     );
   }
